fcc527ad5a811778c7d72e55a7cb435437a28dde
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-01 18:21:02 -0800

    callbackReview#

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,16 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf-8', function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      data = data.split('\n');
+      callback(null, data[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
