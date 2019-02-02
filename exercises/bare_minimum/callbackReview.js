c350c8cc8104477ed0c8cabe1c3328f8039cdda7
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-01 18:47:37 -0800

    completed callbackReview with getStatusCode

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -19,8 +19,15 @@ var pluckFirstLineFromFile = function (filePath, callback) {
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, { json: true }, (err, res, body) => {
+  if (err) {
+     callback(err, null) 
+  } else {
+    callback(null, res.statusCode);
+  }
+});
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
